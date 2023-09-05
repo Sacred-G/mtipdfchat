53374a2988bea34e02d34ca2c5c88d0d31342645
Sacred-G <sgbouldin12@gmail.com>
2023-09-05 08:30:53 -0700

    new file:   .streamlit/secrets.toml

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from pdf2image import convert_from_path
 
 st.set_page_config(page_title="MTI FieldTech AI", layout="wide")
 
+openai_api_key = st.secrets["general"]["OPENAI_API_KEY"]
+
 with st.sidebar:
    
     st.image("Images/mti.png")
