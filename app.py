2151fc9baba59be7f1b8593c8d5b04883b9cd50b
Steven Bouldin <96607187+Sacred-G@users.noreply.github.com>
2023-09-06 03:08:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,23 @@ import pdfplumber
 import streamlit as st
 from streamlit_chat import message
 from agent import Agent
+
 from pdf2image import convert_from_path
 
+# Access the API key from st.secrets
+openai_api_key = st.secrets["openai_api_key"]
 
+# Initialize the Agent with the API key
+agent = Agent(openai_api_key=openai_api_key)
 st.set_page_config(page_title="MTI FieldTech AI", layout="wide")
 
-OPENAI_API_KEY = st.secrets["general"]["OPENAI_API_KEY"]
+st.image("Images/mti.png", caption='MTI Fieldtech Chatbot', width=300)
 
 with st.sidebar:
-   
-    st.image("Images/mti.png")
-
-
+        with st.expander("Settings",  expanded=True):
+            TEMP = st.slider(label="LLM Temperature", min_value=0.0, max_value=1.0, value=0.3)
+            st.markdown("Adjust the LLM Temperature: A higher value makes the output more random, while a lower value makes it more deterministic.")
+            st.markdown("NOTE: Anything above 0.7 may produce hallucinations")
 
 def display_messages():
     st.subheader("Chat")
@@ -25,13 +30,13 @@ def display_messages():
     st.session_state["thinking_spinner"] = st.empty()
 
 def process_input():
-    if st.session_state["user_input"] and len(st.session_state["user_input"].strip()) > 0:
-        user_text = st.session_state["user_input"].strip()
-        with st.session_state["thinking_spinner"], st.spinner(f"Thinking"):
-            agent_text = st.session_state["agent"].ask(user_text)
-        st.session_state["messages"].append((user_text, True))
-        st.session_state["messages"].append((agent_text, False))
-        st.session_state["user_input"] = ""  # Clear the input
+    with st.sidebar:
+        if st.session_state["user_input"] and len(st.session_state["user_input"].strip()) > 0:
+            user_text = st.session_state["user_input"].strip()
+            with st.session_state["thinking_spinner"], st.spinner(f"Thinking"):
+                agent_text = st.session_state["agent"].ask(user_text)
+            st.session_state["messages"].append((user_text, True))
+            st.session_state["messages"].append((agent_text, False))
         
         
         
@@ -71,7 +76,7 @@ def main():
             st.session_state["agent"] = Agent(st.session_state["OPENAI_API_KEY"])
         else:
             st.session_state["agent"] = None
-    with st.sidebar: 
+    with st.sidebar:  
         if st.text_input("OpenAI API Key", value=st.session_state["OPENAI_API_KEY"], key="input_OPENAI_API_KEY", type="password"):
             if (
                 len(st.session_state["input_OPENAI_API_KEY"]) > 0
@@ -93,8 +98,8 @@ def main():
             on_change=read_and_save_file,
             label_visibility="collapsed",
             accept_multiple_files=True)
-            
-        temp_pdf_path = st.session_state.get("temp_pdf_path", "")  # Retrieve the path from session_state
+        
+    temp_pdf_path = st.session_state.get("temp_pdf_path", "")  # Retrieve the path from session_state
 
 
 
@@ -118,15 +123,11 @@ def main():
 
     display_messages()
     st.text_input("Message", key="user_input", disabled=not is_openai_api_key_set(), on_change=process_input)
-    
-    with st.sidebar:
-        with st.expander("Settings",  expanded=True):
-            TEMP = st.slider(label="LLM Temperature", min_value=0.0, max_value=1.0, value=0.3)
-            st.markdown("Adjust the LLM Temperature: A higher value makes the output more random, while a lower value makes it more deterministic.")
-            st.markdown("NOTE: Anything above 0.7 may produce hallucinations")
+
     st.divider()
+    st.markdown("Source code: [Github](https://github.com/sacred-g/chatpdfs)")
     st.markdown("Created by Steven Bouldin")
-    st.markdown("Version: 1.2")
+    st.markdown("Version: 1.1")
 
 
 
